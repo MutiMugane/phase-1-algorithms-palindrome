db8782171706cbc32c952dfb98b459a36b2a45f8
+ <+>
2022-09-05 08:29:30 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+        return false;
+    }
+  }
+  return (true);
 }
+  isPalindrome('Abba');
+
+
+
+
 
 /* 
   Add your pseudocode here
